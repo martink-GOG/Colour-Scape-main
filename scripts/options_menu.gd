6d35c00be3627a1899ab9c40a63c6590748d8a42
EBakker08 <1218556@student.roc-nijmegen.nl>
2026-02-09 10:28:08 +0100

    EoD
    UI is made. Can't test it yet because game starts immediately.

---
+++ scripts/options_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_volume_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_color_blindness_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
